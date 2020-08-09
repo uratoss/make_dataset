e97e7248857e1a3d8575a98922b08d21f4308945
uratoss <adlersschanze@gmail.com>
2020-08-09 11:35:39 +0900

    [change]cabochaを使ったparseでchankごとにリストを返すように変更 & 辞書を指定するように変更
    cabochaで使うmecab辞書をneologdにした

--- parse.py
+++ parse.py
@@ -11,42 +11,52 @@ import subprocess
 # morph = default, use mecab, morphologcal analysis
 # morph_base = default, use mecab, morphologcal analysis with base form
 # phr = default, use mecab, phrase
-def parse(lines,mode = 'morph'):
+def parse(lines, mode="morph"):
     sq = []
-    if mode in {'morph', 'morph_base'}:
-        dicdir = subprocess.run(['mecab-config', '--dicdir'], encoding='utf-8', stdout=subprocess.PIPE).stdout.strip() + '/mecab-ipadic-neologd'
-        m = MeCab.Tagger('-d '+dicdir)
-    elif mode in {'phr'}:
-        c = CaboCha.Parser('-f1')
-    for line in lines.splitlines():
-        if not(line):
-            continue
-        s = []
-        if mode in {'morph', 'morph_base'}:
+    dicdir = (
+        subprocess.run(
+            ["mecab-config", "--dicdir"], encoding="utf-8", stdout=subprocess.PIPE
+        ).stdout.strip()
+        + "/mecab-ipadic-neologd"
+    )
+    if mode in {"morph", "morph_base"}:
+        m = MeCab.Tagger("-d " + dicdir)
+
+        for line in lines.splitlines():
+            if not (line):
+                continue
+            s = []
             for mline in m.parse(line.strip()).splitlines()[:-1]:
-                surface = mline.split('\t')[0]
-                feature = mline.split('\t')[1].split(',')
-                if (feature[0] == '記号') or (mode == 'morph'):
+                surface = mline.split("\t")[0]
+                feature = mline.split("\t")[1].split(",")
+                if (feature[0] == "記号") or (mode == "morph"):
                     s.append(surface)
                 else:
                     s.append(feature[6])
-        elif mode in {'phr'}:
+            sq.append(" ".join(s))
+    elif mode in {"phr"}:
+        c = CaboCha.Parser("-f1 -d" + dicdir)
+        for line in lines.splitlines():
+            if not (line):
+                continue
             tree = c.parse(line)
-            for i in range(0,tree.size()):
+            for i in range(0, tree.size()):
                 token = tree.token(i)
-                text = token.surface if token.chunk else (text + token.surface) 
-                if i == tree.size() - 1 or tree.token(i+1).chunk:
-                    s.append(text)
-        sq.append(' '.join(s))
+                text = token.surface if token.chunk else (text + " " + token.surface)
+                if i == tree.size() - 1 or tree.token(i + 1).chunk:
+                    sq.append(text)
     return sq
- 
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('text_name',help ='text name that you want to processing')
-    parser.add_argument('-o','--out_path',default='./',help='output path of precessed file')
+    parser.add_argument("text_name", help="text name that you want to processing")
+    parser.add_argument(
+        "-o", "--out_path", default="./", help="output path of precessed file"
+    )
     args = parser.parse_args()
 
-    sq = parse(open(args.text_name).read())
-    out_path = os.path.join(args.out_path,os.path.basename(args.text_name))
-    with open(out_path,'w') as f:
-        print('\n'.join(sq),file=f)
+    sq = parse(open(args.text_name).read(), mode="morph")
+    out_path = os.path.join(args.out_path, os.path.basename(args.text_name))
+    with open(out_path, "w") as f:
+        print("\n".join(sq), file=f)
