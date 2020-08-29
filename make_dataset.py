cd8a435a545b85a4ea146e702f0abcc6caaba934
uratoss <adlersschanze@gmail.com>
2020-08-30 02:33:41 +0900

    [change]x:yでデータセットを作るように変更

--- make_dataset.py
+++ make_dataset.py
@@ -10,60 +10,56 @@ import glob
 from parse import parse
 from mkvocab import mkvocab
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('files',help='files that you want to add vocabuary', nargs='*')
-    parser.add_argument('-o','--out_path',default='./',help='output path of precessed file')
+    parser.add_argument("files", help="files that you want to add vocabuary", nargs="*")
+    parser.add_argument(
+        "-o", "--out_path", default="./", help="output path of precessed file"
+    )
     args = parser.parse_args()
 
-    vocab = {'<PAD>':0}
-    rvocab = {0:'<PAD>'}
+    vocab = {"<PAD>": 0}
+    rvocab = {0: "<PAD>"}
     xs = []
     ts = []
     chunk = 1
-    offset = -3
     for file_paths in args.files:
         files = glob.glob(file_paths)
         for file_name in files:
-            parsed =[ f.strip() for f in open(file_name).read().replace("\n","").split("<P>")]
+            parsed = [
+                f.strip() for f in open(file_name).read().replace("\n", "").split("<P>")
+            ]
 
-            head = "<BOT>"
-            for i in range(len(parsed)):
-                x = head + " "
+            for i in range(len(parsed) - (chunk * 2 - 1)):
+                x = ""
                 for j in range(chunk):
-                    if i+j > len(parsed)-1:
-                        break
-                    x = x + parsed[i+j] + " "
-                if i+j+1 > len(parsed)-1:
-                    end_str = "<EOT>"
-                else:
-                    end_str = parsed[i+j+1].split()[0].strip()
-                xs.append(x.strip())
-                ts.append((" ".join(x.split()[1:]) + " " + end_str).strip())
-                x_split = parsed[i].split()
-                head = ""
-                for k in range(max(offset,-len(x_split)),0,1):
-                    head = head + x_split[k] + " "
-                head = head.strip()
-    xs = '\n'.join(xs)
-    ts = '\n'.join(ts)
-    mkvocab(xs,vocab,rvocab)
-    mkvocab(ts,vocab,rvocab)
+                    x += parsed[i + j] + " "
+                xs.append(x)
+                y = ""
+                for j in range(chunk):
+                    y += parsed[i + chunk + j] + " "
+                end_str = (
+                    "<EOT> <EOS>" if (i + chunk + j + 1) > len(parsed) - 1 else "<EOS>"
+                )
+                ts.append(y + end_str)
+    xs = "\n".join(xs)
+    ts = "\n".join(ts)
+    mkvocab(xs, vocab, rvocab)
+    mkvocab(ts, vocab, rvocab)
 
-    out_path = os.path.join(args.out_path,'vocab.txt')
-    with open(out_path,'w') as f:
-        print('\n'.join(vocab),file=f)
-    out_path = os.path.join(args.out_path,'vocab.dump')
-    with open(out_path,'wb') as f:
-        pickle.dump(vocab,f)
-    out_path = os.path.join(args.out_path,'rvocab.dump')
-    with open(out_path,'wb') as f:
-        pickle.dump(rvocab,f)
+    out_path = os.path.join(args.out_path, "vocab.txt")
+    with open(out_path, "w") as f:
+        print("\n".join(vocab), file=f)
+    out_path = os.path.join(args.out_path, "vocab.dump")
+    with open(out_path, "wb") as f:
+        pickle.dump(vocab, f)
+    out_path = os.path.join(args.out_path, "rvocab.dump")
+    with open(out_path, "wb") as f:
+        pickle.dump(rvocab, f)
 
-    out_path = os.path.join(args.out_path,'x.txt')
-    with open(out_path,'w') as f:
-        print(xs,file=f)
-    out_path = os.path.join(args.out_path,'t.txt')
-    with open(out_path,'w') as f:
-        print(ts,file=f)
-
+    out_path = os.path.join(args.out_path, "x.txt")
+    with open(out_path, "w") as f:
+        print(xs, file=f)
+    out_path = os.path.join(args.out_path, "t.txt")
+    with open(out_path, "w") as f:
+        print(ts, file=f)
