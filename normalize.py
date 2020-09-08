93a36fc8f8e8111973e3ff20fadf69b8ca0c7c32
uratoss <adlersschanze@gmail.com>
2020-09-08 23:49:54 +0900

    [clean]整形した

--- normalize.py
+++ normalize.py
@@ -4,14 +4,16 @@ import os
 import argparse
 import neologdn
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('text_name',help ='text name that you want to processing')
-    parser.add_argument('-o','--out_path',default='./',help='output path of precessed file')
+    parser.add_argument("text_name", help="text name that you want to processing")
+    parser.add_argument(
+        "-o", "--out_path", default="./", help="output path of precessed file"
+    )
     args = parser.parse_args()
 
     normalized = neologdn.normalize(open(args.text_name).read())
-    out_path = os.path.join(args.out_path,os.path.basename(args.text_name))
+    out_path = os.path.join(args.out_path, os.path.basename(args.text_name))
     print(normalized)
-    with open(out_path,'w') as f:
-        print(normalized,file=f)
+    with open(out_path, "w") as f:
+        print(normalized, file=f)
